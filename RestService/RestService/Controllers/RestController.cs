4299625b5d6e960d1aafc588b43593682f6873c9
Gloomwidow <michalmachnio.mm@gmail.com>
2025-06-26 11:19:24 +0200

    Added REST service

---
+++ RestService/RestService/Controllers/RestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestController :  ControllerBase
+    {
+        private const int CallsToCauseError = 10;
+
+        private static int CallsCount = 0;
+
+        [HttpGet]
+        [Route("/somedata")]
+        public IActionResult GetSomeData()
+        {
+            CallsCount++;
+            if (CallsCount % CallsToCauseError == 0)
+            {
+                throw new Exception($"somedata not returned due to this being call no. {CallsToCauseError}");
+            }
+
+            return Ok("somedata");
+        }
+    }
+}
